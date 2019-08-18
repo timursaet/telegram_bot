6ecf3212d354636aa6c90ef70eeac6d41aa7370c
timursaet <s.timursaet@yandex.ru>
2019-08-18 19:06:59 +0500

    Bot

---
+++ index.js
@@ -0,0 +1,8 @@
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
+
+const Bot = new TelegramBot(TOKEN, {polling: true})
+
+Bot.on('message', msg => {
+    Bot.sendMessage(msg.chat.id,`HI, "Hi, ${msg.from.first_name}"`)
+});
