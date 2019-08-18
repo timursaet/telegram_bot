94d12dc0afe3f8b2c1faa60138b36e1d1fe81cee
timursaet <s.timursaet@yandex.ru>
2019-08-18 21:12:13 +0500

    bot

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 
-const Bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true});
 
-Bot.on('message', (msg) => {
+bot.on('message', (msg) => {
     //Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
     const chatId = msg.chat.id;
     // send a message to the chat acknowledging receipt of their message
