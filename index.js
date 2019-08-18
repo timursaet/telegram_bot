7940d4243546da5770e39ac8331f2c77523c14b6
timursaet <s.timursaet@yandex.ru>
2019-08-18 19:46:30 +0500

    add msg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 
 const Bot = new TelegramBot(TOKEN, {polling: true});
 
-Bot.on('message', msg => {
+Bot.on('message', (msg) => {
     //Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
     const chatId = msg.chat.id;
     // send a message to the chat acknowledging receipt of their message
