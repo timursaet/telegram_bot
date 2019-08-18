26d186afa7348df02a1d487fd293f041c3253342
timursaet <s.timursaet@yandex.ru>
2019-08-18 19:17:11 +0500

    add how are you

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 const Bot = new TelegramBot(TOKEN, {polling: true})
 
 Bot.on('message', msg => {
-    Bot.sendMessage(msg.chat.id,`HI, "Hi, ${msg.from.first_name}"`)
-});
+    Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
+})
