4f795b5a1b124b1d514c2c16b89a27348be9bb48
timursaet <s.timursaet@yandex.ru>
2019-08-18 19:42:54 +0500

    add ChatID

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 
-const Bot = new TelegramBot(TOKEN, {polling: true})
+const Bot = new TelegramBot(TOKEN, {polling: true});
 
 Bot.on('message', msg => {
-    Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
+    //Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
+    const chatId = msg.chat.id;
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
 })
