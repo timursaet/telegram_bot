3d3192233a898e418eccd277a5a1262c9a27c35a
timursaet <s.timursaet@yandex.ru>
2019-08-18 21:30:50 +0500

    f

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.on('message', msg => {
-    Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
+//bot.on('message', msg => {
+    //Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
     //const chatId = msg.chat.id;
     // send a message to the chat acknowledging receipt of their message
     //bot.sendMessage(chatId, msg);
-})
+    bot.on('message', (msg) => {
+        const chatId = msg.chat.id;
+       
+        // send a message to the chat acknowledging receipt of their message
+        bot.sendMessage(chatId, 'Received your message');
+      });
+//})
