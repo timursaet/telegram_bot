2956c37d2d7584cb07d0d665af81c5fbbdfe5943
timursaet <s.timursaet@yandex.ru>
2019-08-30 17:24:43 +0500

    Deploy

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
+const token = '505804275:AAFuQsOXOvxuIZ4wLT5NbsCRrx_W7J3S7pA';
 
-const bot = new TelegramBot(TOKEN, {polling: true});
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
 
-//bot.on('message', msg => {
-    //Bot.sendMessage(msg.chat.id,`HI, "Hi как дела?, ${msg.from.first_name}"`)
-    //const chatId = msg.chat.id;
-    // send a message to the chat acknowledging receipt of their message
-    //bot.sendMessage(chatId, msg);
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-       
-        // send a message to the chat acknowledging receipt of their message
-        bot.sendMessage(chatId, 'Received your message');
-      });
-//})
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+});
